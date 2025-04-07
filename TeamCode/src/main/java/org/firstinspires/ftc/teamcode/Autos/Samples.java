bcb2f0895ff3f96f4a550749178320f3d89d6532
RadiGamer <radicolagamer@gmail.com>
2025-04-07 03:50:13 -0600

    Fixed Sample Auto variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/Samples.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/Samples.java
@@ -1,20 +1,18 @@
 package org.firstinspires.ftc.teamcode.Autos;
 
-import static org.firstinspires.ftc.teamcode.Config.DrivePos.SamplesStartPose;
-import static org.firstinspires.ftc.teamcode.Config.DrivePos.parkControlPose;
-import static org.firstinspires.ftc.teamcode.Config.DrivePos.parkPose;
-import static org.firstinspires.ftc.teamcode.Config.DrivePos.pickup1Pose;
-import static org.firstinspires.ftc.teamcode.Config.DrivePos.pickup2Pose;
-import static org.firstinspires.ftc.teamcode.Config.DrivePos.pickup3Pose;
-import static org.firstinspires.ftc.teamcode.Config.DrivePos.scorePose;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.sampleParkControlPose;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.sampleParkPose;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.samplePickup1Pose;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.samplePickup2Pose;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.samplePickup3Pose;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.sampleScorePose;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.samplesStartPose;
 
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.ParallelDeadlineGroup;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitCommand;
-import com.pedropathing.follower.Follower;
-import com.pedropathing.localization.Pose;
 import com.pedropathing.pathgen.BezierCurve;
 import com.pedropathing.pathgen.BezierLine;
 import com.pedropathing.pathgen.Path;
@@ -24,28 +22,16 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.Commands.Arm.ArmToPoseCMD;
 import org.firstinspires.ftc.teamcode.Commands.Claw.ClawCloseCMD;
-import org.firstinspires.ftc.teamcode.Commands.Claw.ClawOpenCMD;
 import org.firstinspires.ftc.teamcode.Commands.Compund.ScoreCMD;
 import org.firstinspires.ftc.teamcode.Commands.Compund.TransferCMD;
 import org.firstinspires.ftc.teamcode.Commands.Elev.ElevSARCMD;
 import org.firstinspires.ftc.teamcode.Commands.Elev.ElevToPoseCMD;
 import org.firstinspires.ftc.teamcode.Commands.Extendo.ExtendCMD;
-import org.firstinspires.ftc.teamcode.Commands.Extendo.RetractCMD;
 import org.firstinspires.ftc.teamcode.Commands.intake.intakeInCMD;
 import org.firstinspires.ftc.teamcode.Commands.intake.intakeKeepCMD;
 import org.firstinspires.ftc.teamcode.Commands.wrist.wristDownCMD;
 import org.firstinspires.ftc.teamcode.Commands.wrist.wristUpCMD;
-import org.firstinspires.ftc.teamcode.Config.DrivePos;
 import org.firstinspires.ftc.teamcode.Config.OpModeCommand;
-import org.firstinspires.ftc.teamcode.Subsystems.ArmSubsystem;
-import org.firstinspires.ftc.teamcode.Subsystems.ClawSubsystem;
-import org.firstinspires.ftc.teamcode.Subsystems.ElevatorSubsystem;
-import org.firstinspires.ftc.teamcode.Subsystems.ExtendoSubsystem;
-import org.firstinspires.ftc.teamcode.Subsystems.IntakeSubsystem;
-import org.firstinspires.ftc.teamcode.Subsystems.PedroSubsystem;
-import org.firstinspires.ftc.teamcode.Subsystems.WristSubsystem;
-import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
-import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
 
 @Autonomous(name = "samples", group = "##")
 public class Samples extends OpModeCommand {
@@ -56,49 +42,49 @@ public class Samples extends OpModeCommand {
 
 
     public void createPaths() {
-        scorePreload = new Path(new BezierLine(new Point(SamplesStartPose), new Point(scorePose)));
-        scorePreload.setLinearHeadingInterpolation(SamplesStartPose.getHeading(), scorePose.getHeading());
+        scorePreload = new Path(new BezierLine(new Point(samplesStartPose), new Point(sampleScorePose)));
+        scorePreload.setLinearHeadingInterpolation(samplesStartPose.getHeading(), sampleScorePose.getHeading());
 
         grabPickup1 = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(scorePose), new Point(pickup1Pose)))
+                .addPath(new BezierLine(new Point(sampleScorePose), new Point(samplePickup1Pose)))
                 .setTangentHeadingInterpolation()
 
                 .build();
 
         scorePickup1 = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(pickup1Pose), new Point(scorePose)))
-                .setLinearHeadingInterpolation(pickup1Pose.getHeading(), scorePose.getHeading())
+                .addPath(new BezierLine(new Point(samplePickup1Pose), new Point(sampleScorePose)))
+                .setLinearHeadingInterpolation(samplePickup1Pose.getHeading(), sampleScorePose.getHeading())
                 .build();
 
         grabPickup2 = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(scorePose), new Point(pickup2Pose)))
+                .addPath(new BezierLine(new Point(sampleScorePose), new Point(samplePickup2Pose)))
                 .setTangentHeadingInterpolation()
                 .build();
 
         scorePickup2 = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(pickup2Pose), new Point(scorePose)))
-                .setLinearHeadingInterpolation(pickup2Pose.getHeading(), scorePose.getHeading())
+                .addPath(new BezierLine(new Point(samplePickup2Pose), new Point(sampleScorePose)))
+                .setLinearHeadingInterpolation(samplePickup2Pose.getHeading(), sampleScorePose.getHeading())
                 .build();
 
         grabPickup3 = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(scorePose), new Point(pickup3Pose)))
+                .addPath(new BezierLine(new Point(sampleScorePose), new Point(samplePickup3Pose)))
                 .setTangentHeadingInterpolation()
                 .build();
 
         scorePickup3 = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(pickup3Pose), new Point(scorePose)))
-                .setLinearHeadingInterpolation(pickup3Pose.getHeading(), scorePose.getHeading())
+                .addPath(new BezierLine(new Point(samplePickup3Pose), new Point(sampleScorePose)))
+                .setLinearHeadingInterpolation(samplePickup3Pose.getHeading(), sampleScorePose.getHeading())
                 .build();
 
-        park = new Path(new BezierCurve(new Point(scorePose), /* Control Point */ new Point(parkControlPose), new Point(parkPose)));
-        park.setLinearHeadingInterpolation(scorePose.getHeading(), parkPose.getHeading());
+        park = new Path(new BezierCurve(new Point(sampleScorePose), /* Control Point */ new Point(sampleParkControlPose), new Point(sampleParkPose)));
+        park.setLinearHeadingInterpolation(sampleScorePose.getHeading(), sampleParkPose.getHeading());
 
 
     }
 
     @Override
     public void initialize() {
-        follower.setStartingPose(SamplesStartPose);
+        follower.setStartingPose(samplesStartPose);
 
         createPaths();
 
