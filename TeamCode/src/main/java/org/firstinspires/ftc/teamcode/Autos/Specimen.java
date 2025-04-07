db155263ac64b4e1e0a75714918a61ad3515d41f
RadiGamer <radicolagamer@gmail.com>
2025-04-07 03:45:32 -0600

    Added Specimen Auto - WIP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/Specimen.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode.Autos;
+
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.specimenGoToSample1;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.specimenGoToSample1Control;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.specimenGoToSample2;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.specimenGoToSample3;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.specimenGoToSample3Control;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.specimenGrabSpecimen;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.specimenLeaveSample1;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.specimenLeaveSample2;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.specimenLeaveSample3;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.specimenPark;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.specimenScorePose;
+import static org.firstinspires.ftc.teamcode.Config.DrivePos.specimenStartPose;
+
+import com.pedropathing.pathgen.BezierCurve;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.Path;
+import com.pedropathing.pathgen.Point;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Config.OpModeCommand;
+
+@Autonomous(name = "Specimen", group = "##")
+public class Specimen extends OpModeCommand {
+
+
+    private Path scorePreload, scoreSpecimen, park, grabSpecimen, sample1, sample2, sample3, leaveSample1, leaveSample2, leaveSample3, scoreFromSample3;
+
+
+    public void createPaths() {
+        scorePreload = new Path(new BezierLine(new Point(specimenStartPose), new Point(specimenScorePose)));
+        scorePreload.setConstantHeadingInterpolation(specimenScorePose.getHeading());
+
+        sample1 = new Path(new BezierCurve(new Point(specimenScorePose), new Point(specimenGoToSample1Control), new Point(specimenGoToSample1)));
+        sample1.setLinearHeadingInterpolation(specimenScorePose.getHeading(), specimenGoToSample1.getHeading());
+
+        leaveSample1 = new Path(new BezierLine(new Point(specimenGoToSample1), new Point(specimenLeaveSample1)));
+        leaveSample1.setLinearHeadingInterpolation(specimenGoToSample1.getHeading(), specimenLeaveSample1.getHeading());
+
+        sample2 = new Path(new BezierLine(new Point(specimenLeaveSample1), new Point(specimenGoToSample2)));
+        sample2.setLinearHeadingInterpolation(specimenLeaveSample1.getHeading(), specimenGoToSample2.getHeading());
+
+        leaveSample2 = new Path(new BezierLine(new Point(specimenGoToSample2), new Point(specimenLeaveSample2)));
+        leaveSample2.setLinearHeadingInterpolation(specimenGoToSample2.getHeading(), specimenLeaveSample2.getHeading());
+
+        sample3 = new Path(new BezierCurve(new Point(specimenLeaveSample2), new Point(specimenGoToSample3Control), new Point(specimenGoToSample3)));
+        sample3.setLinearHeadingInterpolation(specimenLeaveSample2.getHeading(), specimenGoToSample3.getHeading());
+
+        leaveSample3 = new Path(new BezierLine(new Point(specimenGoToSample3), new Point(specimenLeaveSample3)));
+        leaveSample3.setLinearHeadingInterpolation(specimenGoToSample3.getHeading(), specimenLeaveSample3.getHeading());
+
+        scoreFromSample3 = new Path(new BezierLine(new Point(specimenLeaveSample3), new Point(specimenScorePose)));
+        scoreFromSample3.setLinearHeadingInterpolation(specimenLeaveSample3.getHeading(), specimenScorePose.getHeading());
+
+        grabSpecimen = new Path(new BezierLine(new Point(specimenScorePose), new Point(specimenGrabSpecimen)));
+        grabSpecimen.setLinearHeadingInterpolation(specimenScorePose.getHeading(), specimenGrabSpecimen.getHeading());
+
+        scoreSpecimen = new Path(new BezierLine(new Point(specimenGrabSpecimen), new Point(specimenScorePose)));
+        scoreSpecimen.setLinearHeadingInterpolation(specimenGrabSpecimen.getHeading(), specimenScorePose.getHeading());
+
+        park = new Path(new BezierLine(new Point(specimenScorePose), new Point(specimenPark)));
+        park.setLinearHeadingInterpolation(specimenScorePose.getHeading(), specimenPark.getHeading());
+
+    }
+
+    @Override
+    public void initialize() {
+        follower.setStartingPose(specimenStartPose);
+
+        createPaths();
+
+        schedule(
+                pedroSubsystem.followPathCmd(scorePreload)
+                        .andThen(pedroSubsystem.followPathCmd(sample1))
+                        .andThen(pedroSubsystem.followPathCmd(leaveSample1))
+                        .andThen(pedroSubsystem.followPathCmd(sample2))
+                        .andThen(pedroSubsystem.followPathCmd(leaveSample2))
+                        .andThen(pedroSubsystem.followPathCmd(sample3))
+                        .andThen(pedroSubsystem.followPathCmd(leaveSample3))
+                        .andThen(pedroSubsystem.followPathCmd(scoreFromSample3))
+                        .andThen(pedroSubsystem.followPathCmd(grabSpecimen))
+                        .andThen(pedroSubsystem.followPathCmd(scoreSpecimen))
+                        .andThen(pedroSubsystem.followPathCmd(grabSpecimen))
+                        .andThen(pedroSubsystem.followPathCmd(scoreSpecimen))
+                        .andThen(pedroSubsystem.followPathCmd(grabSpecimen))
+                        .andThen(pedroSubsystem.followPathCmd(scoreSpecimen))
+                        .andThen(pedroSubsystem.followPathCmd(park))
+        );
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Config/DrivePos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Config/DrivePos.java
@@ -6,18 +6,49 @@ import com.pedropathing.localization.Pose;
 @Config
 public class DrivePos {
 
-    public static Pose SamplesStartPose = new Pose(9, 111, Math.toRadians(270));
+    //Importante: Las variables con cada autonomo comienzan con sample/specimen dependiendo
+    //Posiblemente sea mejor separlas por clases
+
+    //Autonomo Samples
+
+    public static Pose samplesStartPose = new Pose(9, 111, Math.toRadians(270));
+
+    public static Pose sampleScorePose = new Pose(16.37, 127.1, Math.toRadians(315));
+
+    public static Pose samplePickup1Pose = new Pose(25, 125.85  , Math.toRadians(-17));
+
+    public static Pose samplePickup2Pose = new Pose(26.51, 129.18, Math.toRadians(13));
+
+    public static Pose samplePickup3Pose = new Pose(29.5, 134.8, Math.toRadians(30));
+
+    public static Pose sampleParkPose = new Pose(66.02, 99.03, Math.toRadians(90));
+
+    public static Pose sampleParkControlPose = new Pose(55.62, 138.8, Math.toRadians(90));
 
 
-    public static Pose scorePose = new Pose(16.37, 127.1, Math.toRadians(315));
+    //Autonomo Specimen
+    public static Pose specimenStartPose = new Pose(9, 56, Math.toRadians(180));
 
-    public static Pose pickup1Pose = new Pose(25, 125.85  , Math.toRadians(-17));
+    public static Pose specimenScorePose = new Pose(38, 66, Math.toRadians(180));
 
-    public static Pose pickup2Pose = new Pose(26.51, 129.18, Math.toRadians(13));
+    public static Pose specimenGoToSample1 = new Pose(48, 34, Math.toRadians(90));
+    public static Pose specimenGoToSample1Control = new Pose(22, 40);
 
-    public static Pose pickup3Pose = new Pose(29.5, 134.8, Math.toRadians(30));
+    public static Pose specimenLeaveSample1 = new Pose(28, 28, Math.toRadians(55));
 
-    public static Pose parkPose = new Pose(66.02, 99.03, Math.toRadians(90));
+    public static Pose specimenGoToSample2 = new Pose(48, 24, Math.toRadians(90));
 
-    public static Pose parkControlPose = new Pose(55.62, 138.8, Math.toRadians(90));
+    public static Pose specimenLeaveSample2 = new Pose(28, 15, Math.toRadians(55));
+
+    public static Pose specimenGoToSample3 = new Pose(57, 8, Math.toRadians(0));
+    public static Pose specimenGoToSample3Control = new Pose(51, 23);
+
+    public static Pose specimenLeaveSample3 = new Pose(11, 8, Math.toRadians(0));
+
+    public static Pose specimenGrabSpecimen = new Pose(38, 66, Math.toRadians(0));
+
+    public static Pose specimenPark = new Pose(16, 47, Math.toRadians(-135));
+
 }
+
+
